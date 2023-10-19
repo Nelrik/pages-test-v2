21cddceacc911f6ece080ce260efa3287fc871d3
Nelrik <addevel@outlook.com>
2023-10-19 17:36:50 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Alan Diaz';
+
+  ngOnInit(): void {
+    $("header").append("<div class='glitch-window'></div>");
+    //fill div with clone of real header
+    $( "h1.glitched" ).clone().appendTo( ".glitch-window" );
+  }
 }
